a6b335cd6bf72b352622f50d98395fa8e09eab72
Joel Garza <joel.garza@gmail.com>
2016-08-17 15:53:26 -0500

    add env statements for openshift

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
